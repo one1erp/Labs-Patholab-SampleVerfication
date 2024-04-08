2beb9fd08f3cf17a58d0608998a7c75db66e4d62
one1erp <avigaile@one1.co.il>
2024-04-08 17:31:52 +0300

    Initial commit with additional files

---
+++ SampleVerfication/SampleVerficationCls.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace SampleVerfication
+{
+    partial class SampleVerficationCls
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelZ2 = new System.Windows.Forms.Label();
+            this.labelZ1 = new System.Windows.Forms.Label();
+            this.labelMisBdika = new System.Windows.Forms.Label();
+            this.timerFocus = new System.Windows.Forms.Timer(this.components);
+            this.labelResult = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblHeader.Location = new System.Drawing.Point(543, 11);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(412, 84);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "אימות דגימה";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(531, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 35);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 35);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(598, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 43);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "צנצנת 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(123, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 43);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "צנצנת 2";
+            // 
+            // labelZ2
+            // 
+            this.labelZ2.AutoSize = true;
+            this.labelZ2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelZ2.ForeColor = System.Drawing.Color.Red;
+            this.labelZ2.Location = new System.Drawing.Point(113, 106);
+            this.labelZ2.Name = "labelZ2";
+            this.labelZ2.Size = new System.Drawing.Size(169, 32);
+            this.labelZ2.TabIndex = 9;
+            this.labelZ2.Text = "ברקוד לא תקין";
+            this.labelZ2.Visible = false;
+            // 
+            // labelZ1
+            // 
+            this.labelZ1.AutoSize = true;
+            this.labelZ1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelZ1.ForeColor = System.Drawing.Color.Red;
+            this.labelZ1.Location = new System.Drawing.Point(598, 106);
+            this.labelZ1.Name = "labelZ1";
+            this.labelZ1.Size = new System.Drawing.Size(154, 32);
+            this.labelZ1.TabIndex = 10;
+            this.labelZ1.Text = "המקרה בוטל";
+            this.labelZ1.Visible = false;
+            // 
+            // labelMisBdika
+            // 
+            this.labelMisBdika.AutoSize = true;
+            this.labelMisBdika.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelMisBdika.Location = new System.Drawing.Point(263, 200);
+            this.labelMisBdika.Name = "labelMisBdika";
+            this.labelMisBdika.Size = new System.Drawing.Size(0, 44);
+            this.labelMisBdika.TabIndex = 11;
+            // 
+            // timerFocus
+            // 
+            this.timerFocus.Interval = 5000;
+            this.timerFocus.Tick += new System.EventHandler(this.timerFocus_Tick);
+            // 
+            // labelResult
+            // 
+            this.labelResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelResult.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelResult.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.labelResult.ForeColor = System.Drawing.Color.Red;
+            this.labelResult.Location = new System.Drawing.Point(58, 257);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(673, 77);
+            this.labelResult.TabIndex = 12;
+            this.labelResult.Text = "אימות נכשל ";
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelResult.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelMisBdika);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.labelResult);
+            this.panel1.Controls.Add(this.labelZ1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.labelZ2);
+            this.panel1.Location = new System.Drawing.Point(257, 90);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(763, 372);
+            this.panel1.TabIndex = 13;
+            // 
+            // SampleVerficationCls
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblHeader);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "SampleVerficationCls";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(1366, 765);
+            this.Resize += new System.EventHandler(this.SampleVerficationCls_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelZ2;
+        private System.Windows.Forms.Label labelZ1;
+        private System.Windows.Forms.Label labelMisBdika;
+        private System.Windows.Forms.Timer timerFocus;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ SampleVerfication/SampleVerficationCls.cs
@@ -0,0 +1,426 @@
+﻿using LSExtensionWindowLib;
+using LSSERVICEPROVIDERLib;
+using Patholab_Common;
+using Patholab_DAL_V1;
+using Patholab_XmlService;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using System.Configuration;
+using UserControl = System.Windows.Forms.UserControl;
+
+
+namespace SampleVerfication
+{
+   
+
+    [ComVisible(true)]
+    [ProgId("SampleVerfication.SampleVerficationCls")]
+    public partial class SampleVerficationCls : UserControl, IExtensionWindow
+    {
+
+        #region Private fields
+
+        private INautilusProcessXML xmlProcessor;
+        private IExtensionWindowSite2 _ntlsSite;
+        private INautilusServiceProvider sp;
+        private INautilusDBConnection _ntlsCon;
+
+        private DataLayer dal;
+        public bool DEBUG;
+        private string mboxHeader = "מסך אימות דגימה -  Nautilus";
+
+
+
+
+
+
+
+        #endregion
+
+        #region Ctor
+
+        public SampleVerficationCls()
+        {
+            InitializeComponent();
+            BackColor = Color.FromName("Control");
+            this.Dock = DockStyle.Fill;
+
+        }
+
+        #endregion
+
+
+
+        #region Implementation of IExtensionWindow
+
+        public bool CloseQuery()
+        {
+            if (dal != null) dal.Close();
+            this.Dispose();
+            return true;
+        }
+
+        public void Internationalise()
+        {
+        }
+
+        public void SetSite(object site)
+        {
+            _ntlsSite = (IExtensionWindowSite2)site;
+
+            _ntlsSite.SetWindowInternalName("SampleVerfication");
+            _ntlsSite.SetWindowRegistryName("SampleVerfication");
+            _ntlsSite.SetWindowTitle("אימות דגימה");
+            labelResult.Visible = false;
+        }
+
+
+
+
+        public void PreDisplay()
+        {
+
+            xmlProcessor = Utils.GetXmlProcessor(sp);
+
+            Utils.GetNautilusUser(sp);
+
+            InitializeData();
+            timerFocus.Start();
+        }
+
+        public WindowButtonsType GetButtons()
+        {
+            return LSExtensionWindowLib.WindowButtonsType.windowButtonsNone;
+        }
+
+        public bool SaveData()
+        {
+            return false;
+        }
+
+        public void SetServiceProvider(object serviceProvider)
+        {
+            sp = serviceProvider as NautilusServiceProvider;
+            _ntlsCon = Utils.GetNtlsCon(sp);
+
+        }
+
+        public void SetParameters(string parameters)
+        {
+
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public WindowRefreshType DataChange()
+        {
+            return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        }
+
+        public WindowRefreshType ViewRefresh()
+        {
+            return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        }
+
+        public void refresh()
+        {
+        }
+
+        public void SaveSettings(int hKey)
+        {
+        }
+
+        public void RestoreSettings(int hKey)
+        {
+        }
+
+        public void Close()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Initilaize
+
+        public void InitializeData()
+        {
+
+
+            //    Debugger.Launch();
+            try
+            {
+                dal = new DataLayer();
+
+                if (DEBUG)
+                    dal.MockConnect();
+                else
+                    dal.Connect(_ntlsCon);
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+
+                MessageBox.Show("Error in  InitializeData " + "/n" + e.Message, mboxHeader);
+                Logger.WriteLogFile(e);
+            }
+
+        }
+
+
+   
+
+
+
+        #endregion
+
+
+        #region Private methods
+
+
+
+
+
+        #endregion
+
+
+
+
+
+        private void SampleVerficationCls_Resize(object sender, EventArgs e)
+        {
+            lblHeader.Location = new Point(Width / 2 - lblHeader.Width / 2, lblHeader.Location.Y);
+            panel1.Location = new Point(Width / 2 - panel1.Width / 2, panel1.Location.Y);
+        }
+
+
+ 
+        //private void btnExit_Click(object sender, EventArgs e)
+        // {
+        //    ((Form)this.TopLevelControl).Close();
+        //}
+
+
+
+        private void textBox1_KeDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                
+                leaveText1();
+            }
+        }
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBox2_Leave();
+            }   
+            
+        }
+        private void textBox2_Leave(){
+                string sql1 = "select sdg_id from lims_sys.sample where name='" + textBox2.Text.Trim().ToUpper() + "'";
+
+                //var sdgDtls = dal.RunQuery(sql1);
+                decimal? sdgId = dal.GetDynamicDecimal(sql1);
+                if (!(sdgId is null))
+                {
+                    if (textBox1.Text == textBox2.Text)
+                    {
+
+                        if (sdgId != 0)
+                        {
+                        string sql3 = "Select U_Patholab_Number From lims_sys.sdg_user where SDG_ID='" + sdgId.ToString().Trim().ToUpper() + "'";
+
+                        //var sdgDtls = dal.RunQuery(sql1);
+                        string bdika = dal.GetDynamicStr(sql3);
+                        labelMisBdika.Text = "מס' הבדיקה:  " + bdika.Trim();
+                            string sql2 = "select status from lims_sys.sample where name='" + textBox2.Text.Trim().ToUpper() + "'";
+                            string STATUS = dal.GetDynamicStr(sql2);
+                            if (STATUS == "X")
+                            { // סטטוס בוטל
+
+                                textBox1.Text = "";
+                                labelResult.Visible = false;
+                                textBox2.Text = "";
+                                textBox1.Enabled = true;
+                                labelZ1.Visible = false;
+                                labelZ2.Visible = false;
+                                labelZ1.Text = "";
+                                labelZ2.Text = "";
+                            labelResult.Visible = true;
+                            labelResult.Text = "המקרה בוטל ";
+                            labelResult.ForeColor = Color.Red;
+                        }
+                            else
+                            { // עברה
+                                dal.InsertToSdgLog((long)sdgId, "Cyt.Ver", (long)_ntlsCon.GetSessionId(), textBox2.Text.Trim().ToUpper() + " אימות העברה");
+
+                                //textBox1.Text = "בדיקת אימות חדשה";
+                                labelResult.Visible = true;
+                                labelResult.Text = "אימות בוצע ";
+                                labelResult.ForeColor = Color.Green;
+                                textBox1.Text = "";
+                                textBox2.Text = "";
+                                textBox1.Enabled = true;
+                                labelZ2.Visible = false;
+                                labelZ1.Visible = false;
+
+                            }
+
+                        }
+                        else
+                        {// לא מצא
+                         // dal.InsertToSdgLog((long)sdgId, "Cyt.Ver", (long)_ntlsCon.GetSessionId(), "אימות לא העברה");
+
+
+
+                            labelResult.Visible = true;
+                            labelResult.Text = "ברקוד לא נמצא";
+                            labelResult.ForeColor = Color.Red;
+                            textBox1.Text = "";
+                            textBox2.Text = "";
+
+                            textBox1.Enabled = true;
+                            labelZ2.Visible = false;
+                            labelZ1.Visible = false;
+                            labelZ1.Text = "";
+                            labelZ2.Text = "";
+                        }
+
+                    }
+                    else
+                    { // לא זהות
+                    string sql5 = "select sdg_id from lims_sys.sample where name='" + textBox1.Text.Trim().ToUpper() + "'";
+                    decimal? sdgId5 = dal.GetDynamicDecimal(sql5);
+                    dal.InsertToSdgLog((long)sdgId5, "Cyt.Ver", (long)_ntlsCon.GetSessionId(), textBox2.Text.Trim().ToUpper() + "  לבין " + textBox1.Text.Trim().ToUpper() + "  אימות נכשל לבין");
+
+                    labelResult.Visible = true;
+                        labelResult.Text = "אימות נכשל ";
+                        labelResult.ForeColor = Color.Red;
+                        textBox1.Text = "";
+                        textBox1.Enabled = true;
+                        textBox2.Text = "";
+                        labelZ2.Visible = false;
+                        labelZ1.Visible = false;
+                        // labelZ1.Text = "ברקוד לא תקין";
+                        //labelZ2.Text = "ברקוד לא תקין";
+                    }
+                }
+                else
+                { // לא מצא צנצנת
+
+                labelResult.Visible = true;
+                labelResult.Text = "ברקוד לא נמצא";
+                labelResult.ForeColor = Color.Red;
+                textBox1.Enabled = true;
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    labelZ2.Visible = false;
+                    labelZ1.Visible = false;
+                    labelZ1.Text = "";
+                    labelZ2.Text = "";
+                }
+            timerFocus.Start();
+
+        }
+        //private void textBox1_Leave(object sender, EventArgs e)
+        //{
+        //    textBox1.Enabled = false;
+        //    leaveText1();
+        //}
+        private void leaveText1()
+        {
+            
+            string sql1 = "select sdg_id from lims_sys.sample where name='" + textBox1.Text.Trim().ToUpper() + "'";
+
+            //var sdgDtls = dal.RunQuery(sql1);
+            decimal? sdgId = dal.GetDynamicDecimal(sql1);
+            if (!(sdgId is null))
+            {
+                if (sdgId != 0)
+                {
+                    string sql3 = "Select U_Patholab_Number From lims_sys.sdg_user where SDG_ID='" + sdgId.ToString().Trim().ToUpper() + "'";
+
+                    //var sdgDtls = dal.RunQuery(sql1);
+                    string bdika = dal.GetDynamicStr(sql3);
+
+                    labelMisBdika.Text = "מס' הבדיקה:  " + bdika.Trim();
+                    string sql2 = "select status from lims_sys.sample where name='" + textBox1.Text.Trim().ToUpper() + "'";
+                    string STATUS = dal.GetDynamicStr(sql2);
+                    if (STATUS == "X")
+                    { // סטטוס בוטל
+                        labelResult.Visible = true;
+                        labelResult.Text = "המקרה בוטל ";
+                        labelResult.ForeColor = Color.Red;
+
+                        //textBox1.Text = "";
+                        labelResult.Visible = false;
+                        textBox2.Text = "";
+                        //textBox1.Enabled = true;
+                        labelZ1.Visible = false;
+                        labelZ2.Visible = false;
+                        labelZ1.Text = "";
+                        labelZ2.Text = "";
+                        timerFocus.Start();
+                    }
+                    else
+                    {
+                        textBox1.Enabled = false;
+                        labelZ1.Visible = false;
+                        labelZ2.Visible = false;
+                    }
+                }
+
+            }
+            else
+            {
+                labelResult.Visible = true;
+                labelResult.Text = "ברקוד לא נמצא";
+                labelResult.ForeColor = Color.Red;
+                //textBox1.Enabled = false;               
+                textBox2.Text = "";
+                labelZ1.Visible = false;
+                labelZ2.Visible = false;
+                labelZ1.Text = "";
+                timerFocus.Start();
+            }
+        }
+        private void timerFocus_Tick(object sender, EventArgs e)
+        {
+            
+
+            labelResult.Visible = true;
+            labelResult.Text = "בדיקת אימות חדשה";
+            labelResult.ForeColor = Color.Black;
+            labelZ1.Visible = false;
+            labelZ2.Visible = false;
+            labelMisBdika.Text = "";
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox1.Focus();
+            timerFocus.Stop();
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            textBox2_Leave();
+        }
+    }
+}
+
+
+
